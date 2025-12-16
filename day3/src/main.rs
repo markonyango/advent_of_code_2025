cae664ab2549a815bfab323b27ca91fc83123f20
Mark Onyango <mark.onyango@gmail.com>
2025-12-16 16:31:32 +0100

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,17 +3,22 @@
 fn main() {
     let content = std::fs::read_to_string("./day3.txt").unwrap();
     let mut vec = vec![];
+    let mut vec_p2 = vec![];
 
     for line in content.lines() {
         let mut bank = Bank::new(line);
         let joltage = bank.process();
+        let joltage_p2 = bank.process_p2();
 
         vec.push(joltage);
+        vec_p2.push(joltage_p2);
     }
 
     let sum = vec.iter().fold(0, |acc, curr| acc + curr);
+    let sum_p2 = vec_p2.iter().sum::<u64>();
 
     println!("Part1 - sum is {sum}");
+    println!("Part2 - sum is {sum_p2}");
 }
 
 struct Bank {
@@ -36,7 +41,7 @@ impl Bank {
         let (index, value) = vec
             .iter()
             .enumerate()
-            .reduce(|acc, curr| if acc.1 > curr.1 { acc } else { curr })
+            .reduce(|acc, curr| if acc.1 >= curr.1 { acc } else { curr })
             .unwrap();
 
         (index, value)
@@ -55,6 +60,29 @@ impl Bank {
         let res = string.parse::<u32>().unwrap();
 
         res
+    }
+
+    fn process_p2(&self) -> u64 {
+        let mut vec = self.batteries.clone();
+
+        let mut current_index = 0;
+        let mut res = vec![];
+
+        for i in 0..12 {
+            let (index, value) =
+                Bank::find_highest_joltage(&vec[current_index..(&self.batteries.len() - 11 + i)]);
+
+            // trace::info!(index, value);
+            current_index = current_index + index + 1;
+            res.push(value);
+        }
+
+        res.iter()
+            .map(|i| i.to_string())
+            .collect::<Vec<_>>()
+            .join("")
+            .parse::<u64>()
+            .unwrap()
     }
 }
 
@@ -90,5 +118,12 @@ mod tests {
 
         let line = "7383383448669853327488444395431356632533467643668433447312354949444179338343352833433277429334563636";
         assert_eq!(99, process(line));
+    }
+
+    #[test]
+    fn line2() {
+        let line = "987654321111111";
+        let bank = Bank::new(line);
+        assert_eq!(987654321111, bank.process_p2());
     }
 }
